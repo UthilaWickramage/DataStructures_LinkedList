e93897b41f09d8c0df625b41e88fee623a90397c
Uthila Wickramage <uhwickramage@gmail.com>
2024-06-18 21:15:06 +0530

    Made changes to singly linked list

--- src/com/jiat/linkedlists/LinkedList.java
+++ src/com/jiat/linkedlists/LinkedList.java
@@ -1,5 +1,7 @@
 package com.jiat.linkedlists;
 
+import org.w3c.dom.Node;
+
 public class LinkedList {
 
     private Node head;
@@ -19,21 +21,21 @@ public class LinkedList {
         size++;
     }
 
-    public void insert(int value,int index){
-        if(index<0){
+    public void insert(int value, int index) {
+        if (index < 0) {
             throw new IllegalArgumentException("Index cannot be negative");
         }
 
-        if(index == 0){
+        if (index == 0) {
             insertFirst(value);
-        }else{
+        } else {
             Node node = new Node(value);
             Node current = head;
-            for(int i = 0;i<index-1&&current!=null;i++){
+            for (int i = 0; i < index - 1 && current != null; i++) {
                 current = current.next;
             }
 
-            if(current == null){
+            if (current == null) {
                 throw new IndexOutOfBoundsException("Index is out of Bound");
             }
             node.next = current.next;
@@ -49,20 +51,98 @@ public class LinkedList {
         head = node;
     }
 
-@Override
-public String toString (){
-StringBuilder builder = new StringBuilder("[");
-Node current = head;
-while (current!=null){
-    builder.append(current.data);
 
-    if(current.next!=null){
-        builder.append(",");
+    public void delete(int value) {
+        if (head == null) {
+            throw new IllegalArgumentException("No value found");
+        } else {
+            if (head.data == value) {
+                head = head.next;
+            } else {
+                Node current = head;
+                while (current.next != null && current.next.data != value) {
+                    current = current.next;
+                }
+                if (current == null || current.next == null) {
+                    throw new IndexOutOfBoundsException();
+                }
+                current.next = current.next.next;
+
+            }
+        }
     }
-    current = current.next;
-}
-return  builder.append("]").toString();
-}
+
+    public boolean search(int value) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == value) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+
+    public void reverse() {
+        Node current = head, next = null, previous = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        head = previous;
+    }
+
+    public int getSize(){
+        Node current = head;
+        int count = 0;
+        while(current.next!=null){
+            count++;
+        }
+        return count;
+
+    }
+
+    public void deleteByIndex(int index) {
+        if (head == null) {
+            throw new RuntimeException("List is Empty");
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("Index cannot be negative");
+        }
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1 && current != null; i++) {
+                current = current.next;
+            }
+            if (current != null) {
+                current.next = current.next.next;
+            }
+        }
+        size--;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+        Node current = head;
+        while (current != null) {
+            builder.append(current.data);
+
+            if (current.next != null) {
+                builder.append(",");
+            }
+            current = current.next;
+        }
+        return builder.append("]").toString();
+    }
 
 
     private static class Node {
--- src/com/jiat/linkedlists/Test.java
+++ src/com/jiat/linkedlists/Test.java
@@ -6,6 +6,18 @@ LinkedList linkedList = new LinkedList();
 linkedList.insert(20);
 linkedList.insertFirst(10);
 linkedList.insert(30,2);
-        System.out.println(linkedList.toString());
+        System.out.println(linkedList);
+linkedList.delete(20);
+        System.out.println(linkedList);
+
+        linkedList.insert(40);
+        linkedList.insert(50);
+        System.out.println(linkedList);
+        linkedList.deleteByIndex(3);
+        System.out.println(linkedList);
+
+        linkedList.reverse();
+        System.out.println(linkedList);
+            System.out.println(linkedList.getSize());
     }
 }
